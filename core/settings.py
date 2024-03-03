0d5d18f905cafe9c07116f9e3daf4916d7c5f78f
jimogeto <jimogeto@gmail.com>
2024-03-03 10:53:28 +0300

    updated allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.environ.get(
     "SECRET_KEY", "django-insecure-y)%kl+kzp2w2ru)9jl#0)%e)@=qzs__@y877_b@efkc=@amgcf"
 )
 DEBUG = os.environ.get("DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS", "127.0.0.1", "blog-app-zcnz.onrender.com"
+).split(",")
 
 
 # Application definition
