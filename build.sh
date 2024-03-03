7ae29d05c09fc6165f9c7db9e67bd2bc614d0433
jimogeto <jimogeto@gmail.com>
2024-03-03 11:49:19 +0300

    updated build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,10 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate 
+
+# Check if CREATE_SUPERUSER is set
+if [[ $CREATE_SUPERUSER ]]; then
+  # Assuming necessary environment variables are set
+  python manage.py createsuperuser --no-input
+fi
