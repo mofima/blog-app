fa0ff53181d946bbffc71d58978ffe313938673a
jimogeto <jimogeto@gmail.com>
2024-03-04 11:34:12 +0300

    update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # exit on error
 set -o errexit
+
+# Print Bash version
+echo "Bash Version: $BASH_VERSION"
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate 
 
-# Check if CREATE_SUPERUSER is set
-if [[ $CREATE_SUPERUSER ]]; then
-  # Assuming necessary environment variables are set
-  python manage.py createsuperuser --no-input
+echo "Checking if CREATE_SUPERUSER is set..."
+if [ "$CREATE_SUPERUSER" ]; then
+    echo "CREATE_SUPERUSER is set. Proceeding to create superuser..."
+    python manage.py createsuperuser --no-input
+    echo "Superuser creation complete."
+else
+    echo "CREATE_SUPERUSER is not set. Skipping superuser creation."
 fi
